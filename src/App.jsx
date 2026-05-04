a3c79738cef011d4018b547ab01804c238e0f6d8
Elegm92 <elegm92@gmail.com>
2026-05-04 11:40:02 +0200

    feat:add API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
 function App() {
+  useEffect(() => {
+    axios.get("https://api.openweathermap.org/data/2.5/forecast", {
+      params: {
+        q: "Madrid",
+        appid: API_KEY,
+        units: "metric",
+        lang: "es",
+      },
+    }).then(({ data }) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Weather App</h1>
