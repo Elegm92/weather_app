5a2d7d73a5711c668c16ec8a94cd062bc6a90e61
Elegm92 <elegm92@gmail.com>
2026-05-06 20:38:24 +0200

    feat:add css to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherList from "./WeatherList/WeatherList";
 import SearchForm from "./SearchForm/SearchForm";
+import "./App.css";
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -33,7 +34,7 @@ function App() {
   }, [city]);
 
   return (
-    <div>
+    <div className="app">
       <h1>Weather App</h1>
       <SearchForm onSearch={(city)=> setCity(city)} />
       {Object.keys(forecast).length > 0 && <WeatherList forecast={forecast}/>}
